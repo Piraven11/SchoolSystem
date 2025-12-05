3d0f679f993c49222a52293deee768c9b4eae639
2563895 <2563895@edu.vaniercollege.qc.ca>
2025-12-05 08:52:06 -0500

    Create the adress class

---
+++ src/main/java/org/piraven/Address.java
@@ -0,0 +1,9 @@
+package org.piraven;
+
+public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String zipCode;
+}
