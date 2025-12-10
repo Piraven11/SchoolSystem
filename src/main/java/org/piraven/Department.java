498df0ab54cef95d93b77fc666503605650bec73
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2025-12-09 23:54:11 -0500

    Created the isDepartmentNameValid method to department class

--- src/main/java/org/piraven/Department.java
+++ src/main/java/org/piraven/Department.java
@@ -1,6 +1,34 @@
 package org.piraven;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
 public class Department {
     private String departmentId;
     private String departmentName;
+
+    public static int nextId = 1;
+
+    /**
+     * Checks if the department name is valid or not
+     * @param departmentName the input department name
+     * @return the department name is valid or not
+     */
+    private static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+
+        for (char c : departmentName.toCharArray()) {
+            if (!Character.isLetter(c) || c != ' ') {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
