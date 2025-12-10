b23dad17086db6fda2aee7800aa2a55e8fc78913
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2025-12-09 22:42:41 -0500

    Added the isPostlCodeValid helper method in the Address class

--- src/main/java/org/piraven/Address.java
+++ src/main/java/org/piraven/Address.java
@@ -1,9 +1,39 @@
 package org.piraven;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Address {
     private int streetNo;
     private String street;
     private String city;
     private Province province;
-    private String zipCode;
+    private String postalCode;
+
+    private static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null || postalCode.length() != 6) {
+            return false;
+        }
+
+        for (int i = 0; i < postalCode.length(); i++) {
+            char c = postalCode.charAt(i);
+            if (c % 2 == 0) {
+                if (!Character.isLetter(c)) {
+                    return false;
+                }
+            } else {
+                if (!Character.isDigit(c)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
