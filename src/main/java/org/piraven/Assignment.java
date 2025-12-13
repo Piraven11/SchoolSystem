9d12acb74c9799b0b81c859be4568501e9615350
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2025-12-13 00:19:50 -0500

    Add generateRandomScore method in assignment class

--- src/main/java/org/piraven/Assignment.java
+++ src/main/java/org/piraven/Assignment.java
@@ -2,6 +2,7 @@ package org.piraven;
 
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Assignment {
     private String assignmentId;
@@ -21,4 +22,24 @@ public class Assignment {
         double avg = sum / scores.size();
     }
 
+    public void generateRandomScores() {
+        Random random = new Random();
+
+        int randomValue = random.nextInt(0, 10 + 1);
+        int randomScore;
+
+        if (randomValue == 0) {
+            randomScore = random.nextInt(0, 60 + 1);
+        } else if (randomValue == 1 || randomValue == 2) {
+            randomScore = random.nextInt(60, 70 + 1);
+        } else if (randomValue == 3 || randomValue == 4) {
+            randomScore = random.nextInt(70, 80 + 1);
+        } else if (randomValue == 5 || randomValue == 6 || randomValue == 7 || randomValue == 8) {
+            randomScore = random.nextInt(70, 80 + 1);
+        } else {
+            randomScore = random.nextInt(90, 100 + 1);
+        }
+
+        scores.add(randomScore);
+    }
 }
