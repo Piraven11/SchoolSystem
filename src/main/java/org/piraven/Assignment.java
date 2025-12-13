0d1b5f020458433d3783068101d8b6529d46b114
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2025-12-12 22:32:38 -0500

    Create Assignment class

---
+++ src/main/java/org/piraven/Assignment.java
@@ -0,0 +1,14 @@
+package org.piraven;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private ArrayList<Integer> scores;
+
+    private static int nextId = 1;
+
+}
