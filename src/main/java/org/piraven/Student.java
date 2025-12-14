f90089a09c8aa2f60a4f118e9b58bad77e73e0b4
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2025-12-14 00:08:37 -0500

    Add the dropCourse method in the student class

--- src/main/java/org/piraven/Student.java
+++ src/main/java/org/piraven/Student.java
@@ -52,4 +52,21 @@ public class Student {
 
         return true;
     }
+
+    /**
+     * Takes the input course and removes that course from the student's registeredCourses list
+     * and remove the student from the course's registered students list
+     * @param course the input course
+     * @return if the course is removed from the student's registered courses list or not
+     */
+    public boolean dropCourse(Course course) {
+        if (registeredCourses.contains(course)) {
+            return false;
+        }
+
+        registeredCourses.remove(course);
+        course.getRegisteredStudents().remove(this);
+
+        return true;
+    }
 }
