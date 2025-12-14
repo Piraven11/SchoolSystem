7f3c832ae4ecd5202f4534aa8c5528f16d259201
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2025-12-14 16:09:31 -0500

    Add the java doc to assignment class

---
+++ src/test/java/org/piraven/AddressTest.java
@@ -0,0 +1,17 @@
+package org.piraven;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AddressTest {
+
+    @Test
+    @DisplayName("")
+    void TestisPostalCodeValid() {
+        String postalCode = "12345";
+        String expectedPostalCode = "false";
+        String actualPostalCode = Address.class.
+    }
+
+
+}
