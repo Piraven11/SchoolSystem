acc00cae2c0e6cb09844babb6e0731c059e1b34c
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2025-12-13 23:12:42 -0500

    Add constructor to student class

--- src/main/java/org/piraven/Student.java
+++ src/main/java/org/piraven/Student.java
@@ -4,6 +4,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @EqualsAndHashCode
 @Setter
 @Getter
@@ -21,8 +23,12 @@ public class Student {
         MALE, FEMALE
     }
 
-    public boolean registerCourse(Course course) {
-
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentId = String.format("S%06d", nextId++);
+        this.studentName = Util.toTitleCase(studentName);
+        this.gender = gender;
+        this.address = address;
+        this.registeredCourses = new ArrayList<>();
+        this.department = department;
     }
-
 }
