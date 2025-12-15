eaec508f7e4336cc57bb6d15587bec027a4b0884
Piraven11 <2563895@edu.vaniercollege.qc.ca>
2025-12-14 21:48:01 -0500

    Remove codes in main

--- src/main/java/org/piraven/Main.java
+++ src/main/java/org/piraven/Main.java
@@ -2,10 +2,5 @@ package org.piraven;
 
 public class Main {
     public static void main(String[] args) {
-        Department department = new Department("lol is");
-        System.out.println(department);
-
-        Course course = new Course("Lol", 2.5, department);
-        System.out.println(course);
     }
 }
